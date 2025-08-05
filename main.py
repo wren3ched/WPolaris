476d98954099d72bdc06f88651b3f577061e5f47
kiLkSS <xlander894@gmail.com>
2025-08-05 01:41:48 -0300

    04/08/25

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from database import Base, engine
 from routes import auth, vault
+from fastapi.middleware.cors import CORSMiddleware
+
+
 
 app = FastAPI(title="Polaris WebApp")
 
@@ -9,7 +12,13 @@ Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router, prefix="/auth")
 app.include_router(vault.router)
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://wpolaris-2.onrender.com"],  # o ["*"] para pruebas
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 async def root():
     return {"message": "Bienvenido a Polaris WebApp"}
